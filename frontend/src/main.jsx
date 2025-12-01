1fea172bf2af6a98147789181bee8fef4b813f3e
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2025-12-01 06:57:31 -0500

    refactoring global styling and polishing ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,9 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { CartProvider } from './context/CartContext.jsx'
 import { StoreProvider } from "./context/StoreContext";
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
